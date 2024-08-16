3588b6eaf06c2e71c0573e88371e33dba5b52cec
AugustoCVS <guty.vertolis@gmail.com>
2024-08-15 23:46:04 -0300

    fix: update search bar

--- src/components/SeachBar/SearchBar.jsx
+++ src/components/SeachBar/SearchBar.jsx
@@ -1,21 +1,27 @@
 import { useState } from "react";
 import "./searchbar.css";
-import { products } from "../../utils/products";
-// import useDebounce from "../../hooks/useDebounce";
-const SearchBar = ({ setFilterList }) => {
-  const [searchWord, setSearchWord] = useState(null);
-  // const debounceSearchWord = useDebounce(searchWord, 300);
+
+const SearchBar = ({ products, setFilterList }) => {
+  const [searchWord, setSearchWord] = useState("");
+
   const handelChange = (input) => {
-    setSearchWord(input.target.value);
+    const value = input.target.value;
+    setSearchWord(value);
     setFilterList(
       products.filter((item) =>
-        item.productName?.toLowerCase().includes(searchWord?.toLowerCase())
+        item.name?.toLowerCase().includes(value.toLowerCase())
       )
     );
   };
+
   return (
     <div className="search-container">
-      <input type="text" placeholder="Search..." onChange={handelChange} />
+      <input
+        type="text"
+        placeholder="Search..."
+        value={searchWord}
+        onChange={handelChange}
+      />
       <ion-icon name="search-outline" className="search-icon"></ion-icon>
     </div>
   );
