d2e097a53d5ca717f23ba27bbc044dae64dc7025
AugustoCVS <guty.vertolis@gmail.com>
2024-08-15 23:28:59 -0300

    feat: add handleGetProducts

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,18 +1,41 @@
 import { Col, Container, Row } from "react-bootstrap";
 import FilterSelect from "../components/FilterSelect";
 import SearchBar from "../components/SeachBar/SearchBar";
-import { Fragment, useState } from "react";
+import { Fragment, useCallback, useEffect, useState } from "react";
 import { products } from "../utils/products";
 import ShopList from "../components/ShopList";
 import Banner from "../components/Banner/Banner";
 import useWindowScrollToTop from "../hooks/useWindowScrollToTop";
-import { useSelector } from "react-redux";
+import { ProductService } from "../services/routes/products";
 
 const Shop = () => {
-  const [filterList, setFilterList] = useState(
-    products.filter((item) => item.category === "sofa")
-  );
+  const [loading, setLoading] = useState(false);
+  const [productsList, setProductsList] = useState([]);
+
+  const handleGetProducts = useCallback(async () => {
+    setLoading(true);
+    await ProductService.getProducts()
+      .then((res) => {
+        setProductsList(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  useEffect(() => {
+    handleGetProducts();
+  }, []);
+
+  const [filterList, setFilterList] = useState([]);
   useWindowScrollToTop();
+
+  useEffect(() => {
+    setFilterList(productsList);
+  }, [productsList]);
 
   return (
     <Fragment>
@@ -21,15 +44,21 @@ const Shop = () => {
         <Container className="filter-bar-contianer">
           <Row className="justify-content-center">
             <Col md={4}>
-              <FilterSelect setFilterList={setFilterList} />
+              <FilterSelect
+                products={productsList}
+                setFilterList={setFilterList}
+              />
             </Col>
             <Col md={8}>
-              <SearchBar setFilterList={setFilterList} />
+              <SearchBar
+                products={productsList}
+                setFilterList={setFilterList}
+              />
             </Col>
           </Row>
         </Container>
         <Container>
-          <ShopList productItems={filterList} />
+          <ShopList productItems={filterList} isLoading={loading} />
         </Container>
       </section>
     </Fragment>
